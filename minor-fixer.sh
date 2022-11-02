b3454d65afd4413b41c54b690591eea10b67bb79
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2022-11-02 21:34:14 +0800

    Update minor-fixer.sh

--- minor-fixer.sh
+++ minor-fixer.sh
@@ -25,6 +25,6 @@ if [ -d "$ispc-ai" ]; then
 	user=root
 	password=$GENPW # change to password given by ISPConfig AI accordingly
 	database=mysql
-	# change  NEWPASSWORD to your  preferred password
-	mysql --user="$user" --password="$password" --database="$database" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '$PANELPW';"
+	# change ChangeMePW to your preferred password
+	mysql --user="$user" --password="$password" --database="$database" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'ChangeMePW';"
 fi
