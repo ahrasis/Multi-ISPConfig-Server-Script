92b64321b2d5f9d99f2952d3864f58be0e528cbe
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2022-11-02 10:46:38 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -69,3 +69,5 @@ certbot certonly \
 wget -O - https://get.ispconfig.org | sh -s -- --use-nginx --unattended-upgrades --use-certbot --no-mail --no-dns --interactive
 
 # Do not run ISPConfig until you change the root password for mysql and use fixed installer_base.lib.php which you must do by opening another CLI interface via ssh
+
+ufw allow from 192.168.0.0/24 to any port 3306 proto tcp
