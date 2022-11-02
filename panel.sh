b533a03ea1b16957b4e76dde4c7d894acb9ffa05
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2022-11-02 14:47:24 +0800

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -3,28 +3,34 @@
 # export DEBIAN_FRONTEND=noninteractive
 # uncomment the above on Ubuntu 22.04
 
+# PLEASE CHANGE ALL THESE VARIABLES TO SUIT YOURS
+HOST=ispc
+DOMAIN=server.tld
+OLIPV4=192.168.0.200 # normally based on your vm's image ip
+NUIPV4=192.168.0.201
+OLIPV6=fe80::ac:76ff:fe2c:166e  # normally based on your vm's image ip
+DNSKEY=XXXXXXXXXXXXXXXXXXXXXX
+DNSMAIL=$HOST@ispc.server.tld
+NEWPW=nEwPw
+
 # change all these ips and hostnames or rename or comment them out accordingly
 cat <<EOF > temp.txt
-192.168.0.201	ispc.server.tld		ispc
-192.168.0.202	web01.server.tld	web01
-192.168.0.203	mx1.server.tld		mx1
-192.168.0.204	mx2.server.tld		mx2
-192.168.0.205	ns1.server.tld		ns1
-192.168.0.206	ns2.server.tld		ns2
-192.168.0.207	mail.server.tld		mail
+192.168.0.201	ispc.$DOMAIN	ispc
+192.168.0.202	web01.$DOMAIN	web01
+192.168.0.203	mx1.$DOMAIN	mx1
+192.168.0.204	mx2.$DOMAIN	mx2
+192.168.0.205	ns1.$DOMAIN	ns1
+192.168.0.206	ns2.$DOMAIN	ns2
+192.168.0.207	mail.$DOMAIN	mail
 EOF
 sed -i -e "/.*192.*/r temp.txt" -e "//d" /etc/hosts
 cat /etc/hosts
 rm temp.txt
 
-HOST=ispc
 echo $HOST > /etc/hostname
 hostname $HOST
 
 # change all these ips or comment out accordingly of you don't have any of them
-OLIPV4=192.168.0.200 # normally based on your vm's image ip
-NUIPV4=192.168.0.201
-OLIPV6=fe80::ac:76ff:fe2c:166e  # normally based on your vm's image ip
 NUIPV6=$(/usr/bin/ip a | sed '/inet6/!d; /2001/d;  /dadfailed/d; /host/d; s/.*inet6 //; s_/.*__p; d')
 sed -i "s/#OLIPV4/$IPV4/" /etc/netplan/01-netcfg.yaml
 sed -i "s/#OLIPV6/$NUIPV6/" /etc/netplan/01-netcfg.yaml
@@ -36,9 +42,9 @@ ln -s dhparam4096.pem dhparams.pem
 ln -s dhparam4096.pem pure-ftpd-dhparams.pem
 
 mkdir -p .secrets
-cat <<EOF > .secrets/$HOST.server.tld.ini
-dns_cloudflare_api_key = xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-dns_cloudflare_e= $HOST.server.tld@gmail.com
+cat <<EOF > .secrets/$HOST.$DOMAIN.ini
+dns_cloudflare_api_key = $DNSKEY
+dns_cloudflare_email = $DNSMAIL
 EOF
 chmod 600 .secrets -R
 
@@ -57,16 +63,16 @@ chmod +x /usr/local/bin/letsencrypt_renew_hook.sh
 
 certbot certonly \
   --dns-cloudflare \
-  --dns-cloudflare-credentials /etc/ssl/private/.secrets/$HOST.server.tld.ini \
+  --dns-cloudflare-credentials /etc/ssl/private/.secrets/$HOST.$DOMAIN.ini \
   --server https://acme-v02.api.letsencrypt.org/directory \
   --agree-tos \
   --non-interactive \
-  --email $HOST@server.tld \
+  --email $DNSMAIL \
   --no-eff-email \
   --rsa-key-size 4096 \
   --renew-hook letsencrypt_renew_hook.sh \
-  --cert-name $HOST.server.tld \
-  -d $HOST.server.tld
+  --cert-name $HOST.$DOMAIN \
+  -d $HOST.$DOMAIN
 
 cat <<EOF > /etc/init.d/installer-lib-temporary-fixer.sh
 #!/bin/sh
@@ -96,7 +102,7 @@ user=root
 password=$GENPW # change to password given by ISPConfig AI accordingly
 database=mysql
 # change  NEWPASSWORD to your  preferred password
-mysql --user="$user" --password="$password" --database="$database" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'NEWPASSWORD';"
+mysql --user="$user" --password="$password" --database="$database" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEWPW';"
 EOF
 chmod +x /etc/init.d/installer-lib-temporary-fixer.sh
 
