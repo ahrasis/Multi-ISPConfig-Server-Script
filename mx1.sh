7f649488aec942729cb03557dac2446ea557a171
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2022-11-02 13:28:20 +0800

    Update mx1.sh

--- mx1.sh
+++ mx1.sh
@@ -47,6 +47,7 @@ snap install core; snap refresh core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 ln -s /snap/bin/certbot /usr/local/bin/certbot
+apt -y install python3-pip
 pip3 install certbot-dns-cloudflare
 
 cd /tmp
